1bda98b8c200993acde84e472a2ae1fa6d0a7912
Alessandro <alessandrocosta103@gmail.com>
2024-12-04 15:44:13 -0300

    projeto finalizado

--- src/main/java/model/Moedas.java
+++ src/main/java/model/Moedas.java
@@ -6,7 +6,6 @@ public class Moedas {
     private double quantidade;
     private String base_code;
     private String target_code;
-    private double conversion_rate;
     private double conversion_result;
     private String time_last_update_utc;
 
@@ -18,7 +17,6 @@ public class Moedas {
         this.quantidade = quantidade;
         this.base_code = moedaRecord.base_code();
         this.target_code = moedaRecord.target_code();
-        this.conversion_rate = Double.valueOf(moedaRecord.conversion_rate());
         this.conversion_result = Double.valueOf(moedaRecord.conversion_result());
         this.time_last_update_utc = moedaRecord.time_last_update_utc();
     }
